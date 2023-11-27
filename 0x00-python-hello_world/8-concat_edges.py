e1bd1608c9fe818d3b87445ad93645bf3b517d6e
ritchie254 <richardoduor012@gmail.com>
2023-11-27 04:14:04 -0800

    python

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+       language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[113:118] + str[:6]
+print(str)
