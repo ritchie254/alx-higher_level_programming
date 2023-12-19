13714f2e711c85ceca582396028fa06d4748b709
ritchie254 <richardoduor012@gmail.com>
2023-12-19 07:43:36 -0800

    python class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square module.
+This module contains a class that defines a square.
+"""
+
+
+class Square():
+    """Defines a square."""
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square module.
+This module contains a class that defines a square and init method that
+sets its size.
+"""
+
+
+class Square():
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Sets the necessary attributes for the Square object.
+        Args:
+            size (int): the size of one edge of the square.
+        """
+        self.__size = size
