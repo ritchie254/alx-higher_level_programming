ef8e9e801f9cb103543f394e70f66a3579f43a13
ritchie254 <richardoduor012@gmail.com>
2024-03-27 09:23:32 -0700

    python networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-# check if arguement is provided
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 url"
-	exit 1
-else
-	URL=$1
-	response=$(curl -s "$URL" -o /tmp/file1)
-	size=$(stat --format=%s /tmp/file1)
-	echo "$size"
-fi
-rm -f /tmp/file1
-
+curl -so /dev/null -w '%{size_download}\n' "$1"
