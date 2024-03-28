b667aac48924c937af51754b2f782413491076df
ritchie254 <richardoduor012@gmail.com>
2024-03-28 13:46:20 -0700

    python networking

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-Python script that takes your GitHub credentials 
+Python script that takes your GitHub
 """
 
 import requests
 import sys
 
 if __name__ == "__main__":
-    userName = sys.argv[1]
-    passWord = sys.argv[2]
+    user = sys.argv[1]
+    passw = sys.argv[2]
 
-    req = requests.get("https://api.github.com/user", auth=(userName, passWord))
+    req = requests.get("https://api.github.com/user", auth=(user, passw))
     print(req.json().get("id"))
