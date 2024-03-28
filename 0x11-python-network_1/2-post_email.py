bc5f2f4d61da3b6fa59efaa4091c185fa7ef092d
ritchie254 <richardoduor012@gmail.com>
2024-03-28 09:06:23 -0700

    python networking

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, sends a POST request 
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    data = dict()
+    data["email"] = sys.argv[2]
+    data = urllib.parse.urlencode(data)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        value = response.read()
+        print("{}".format(value.decode('utf-8')))
