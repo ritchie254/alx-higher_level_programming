cce888e342799827d8c5cdeb6f0d9f4f1fd73582
ritchie254 <richardoduor012@gmail.com>
2024-03-28 14:47:57 -0700

    python networking

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,9 +10,11 @@ if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
     url = "https://api.github.com/repos/"
-    url += owner + '/' + repo + '/commits?per_page=10'
+    url += owner + '/' + repo + '/commits'
     req = requests.get(url)
     for i in range(len(req.json())):
+        if i == 10:
+            break
         data1 = req.json()[i].get("sha")
         commit = req.json()[i].get("commit").get("author").get("name")
         print("{}: {}".format(data1, commit))
