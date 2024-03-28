1a15066a9594c3d50821c41b31f1b544f1d7a074
ritchie254 <richardoduor012@gmail.com>
2024-03-28 10:14:08 -0700

    python networking

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,9 +6,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    try:
-        req = requests.get(sys.argv[1])
-        print(req.text)
-    except Exception as e:
+    req = requests.get(sys.argv[1])
         if req.status_code >= 400:
             print("Error code: {}".format(req.status_code))
+        else:
+            print(req.text)
