9b4044c6addcfa11f9a400b7e05eae28e281cfa5
ritchie254 <richardoduor012@gmail.com>
2024-03-28 13:31:22 -0700

    python networking

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,6 @@ Python script that takes in a letter and sends a rrquest
 
 import sys
 import requests
-import json
 
 if __name__ == "__main__":
     data = {"q": sys.argv[1] if len(sys.argv) > 1 else ""}
